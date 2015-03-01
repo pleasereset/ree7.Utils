ed84f7f447f2758d89d20f86497edcc25f50d165
Pierre BELIN (@Janus) <pierre@ree7.fr>
2015-03-01 20:39:53 +0100

    Added AnimationHelper

---
+++ src/trunk/Helpers/AnimationHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace ree7.Utils.Helpers
+{
+	public static class AnimationHelper
+	{
+		public static Task BeginAsync(Storyboard sb)
+		{
+			TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+			EventHandler<object> completionCallback = null;
+			completionCallback = (s, e) =>
+			{
+				((Storyboard)s).Completed -= completionCallback;
+				tcs.SetResult(null);
+			};
+			sb.Completed += completionCallback;
+			sb.Begin();
+
+			return tcs.Task;
+		}
+	}
+}
