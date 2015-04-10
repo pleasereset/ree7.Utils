5f9686c125976355377f9a849fa7589b1b243408
Pierre BELIN (@Janus) <pierre@ree7.fr>
2015-04-10 15:31:30 +0200

    ApplicationBarPadding changed to SystemChromeVerticalPadding
    Naming reflects the fact that the bottom Navigation Bar is also taken
    into account for phones without physical navigation buttons.

---
+++ src/phone/Controls/SystemChromeVerticalPadding.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+namespace ree7.Utils.Controls
+{
+	/// <summary>
+	/// Provides backwards compatibility for apps relying on ApplicationBarVerticalPadding
+	/// that has been renamed to SystemChromeVerticalPadding
+	/// </summary>
+	[Obsolete]
+	public sealed class ApplicationBarVerticalPadding : SystemChromeVerticalPadding
+	{
+
+	}
+
+	/// <summary>
+	/// Creates an element whose height is the same as the Bottom ApplicationBar + the 
+	/// eventual navigation bar (software buttons).
+	/// Useful for padding a ListView/GridView.
+	/// </summary>
+	public class SystemChromeVerticalPadding : FrameworkElement
+	{
+		public SystemChromeVerticalPadding()
+		{
+			this.Loaded += OnLoaded;
+			this.Unloaded += OnUnloaded;
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			ApplicationView.GetForCurrentView().VisibleBoundsChanged += OnVisibleBoundsChanged;
+			Update();
+		}
+
+		private void OnUnloaded(object sender, RoutedEventArgs e)
+		{
+			ApplicationView.GetForCurrentView().VisibleBoundsChanged -= OnVisibleBoundsChanged;
+		}
+
+		private void OnVisibleBoundsChanged(ApplicationView sender, object args)
+		{
+			Update();
+		}
+
+		private void Update()
+		{
+			var applicationView = ApplicationView.GetForCurrentView();
+			var coreWindow = CoreWindow.GetForCurrentThread();
+
+			if (applicationView.Orientation == ApplicationViewOrientation.Portrait)
+			{
+				// ApplicationBar/chrome on bottom 
+				var height = Math.Abs(coreWindow.Bounds.Bottom - applicationView.VisibleBounds.Bottom);
+
+				this.Width = 1;
+				this.Height = height;
+			}
+			else if (applicationView.Orientation == ApplicationViewOrientation.Landscape)
+			{
+				// ApplicationBar/chrome on the left/right, no vertical padding needed
+				this.Height = 0;
+				this.Width = 0;
+			}
+		}
+	}
+}
